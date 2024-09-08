6d9c768fdabc5b5fc9b4a2aa6e4404d68db1aabf
ApacheTech Solutions <p_matthews@live.co.uk>
2024-09-08 13:19:51 +0100

    git update-index --really-refresh

---
+++ src/Gantry/Core/Brighter/Common/BroadcastMessageToAllPlayersHandler.cs
@@ -0,0 +1,29 @@
+﻿using ApacheTech.Common.BrighterSlim;
+using Gantry.Core.Annotation;
+using Gantry.Core.Brighter.Filters;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+using Vintagestory.Server;
+
+namespace Gantry.Core.Brighter.Common;
+
+/// <summary>
+///     Broadcasts a message to all players on the server.
+/// </summary>
+[ServerSide]
+internal class BroadcastMessageToAllPlayersHandler(ServerMain game) : RequestHandler<BroadcastMessageToAllPlayersCommand>
+{
+    [Side(EnumAppSide.Server, asynchronous: false)]
+    public override BroadcastMessageToAllPlayersCommand Handle(BroadcastMessageToAllPlayersCommand command)
+    {
+        foreach (var player in game.AllOnlinePlayers.Cast<IServerPlayer>())
+        {
+            var message = command.LocaliseForEachPlayer
+                ? Lang.GetL(player.LanguageCode, command.MessageCode, command.Arguments)
+                : Lang.Get(command.MessageCode, command.Arguments);
+            game.SendMessage(player, GlobalConstants.AllChatGroups, message, EnumChatType.Notification);
+        }
+        return base.Handle(command);
+    }
+}
---
+++ src/Gantry/Core/Extensions/DotNet/ObjectExtensions.cs
@@ -0,0 +1,98 @@
+﻿using System.Reflection;
+
+namespace Gantry.Core.Extensions.DotNet;
+
+/// <summary>
+///     Extension methods to aid working with objects.
+/// </summary>
+public static class ObjectExtensions
+{
+    /// <summary>
+    ///     Copies the properties from the source object into the target object.
+    ///     Only properties that have both a public getter and setter will be copied.
+    /// </summary>
+    /// <param name="target">The target object where the properties will be copied to.</param>
+    /// <param name="source">The source object from which the properties will be copied.</param>
+    public static void CopyFrom<TBase, TTo, TFrom>(this TTo target, TFrom source)
+        where TFrom : TBase
+        where TTo : TBase
+    {
+        if (target == null) throw new ArgumentNullException(nameof(target));
+        if (source == null) throw new ArgumentNullException(nameof(source));
+
+        // Get all public properties from the type T
+        var properties = typeof(TBase).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+        foreach (PropertyInfo property in properties)
+        {
+            // Ensure the property has both a public getter and setter
+            if (property.CanRead && property.CanWrite)
+            {
+                // Get the value from the source object
+                object value = property.GetValue(source);
+
+                // Set the value to the target object
+                property.SetValue(target, value);
+            }
+        }
+    }
+
+    /// <summary>
+    ///     Copies the properties from the source object into the target object.
+    ///     Only properties that have both a public getter and setter will be copied.
+    /// </summary>
+    /// <param name="target">The target object where the properties will be copied to.</param>
+    /// <param name="source">The source object from which the properties will be copied.</param>
+    public static void CopyFrom<TDerived, TBase>(this TDerived target, TBase source)
+        where TDerived : TBase
+    {
+        if (target == null) throw new ArgumentNullException(nameof(target));
+        if (source == null) throw new ArgumentNullException(nameof(source));
+
+        // Get all public properties from the type T
+        var properties = typeof(TBase).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+        foreach (PropertyInfo property in properties)
+        {
+            // Ensure the property has both a public getter and setter
+            if (property.CanRead && property.CanWrite)
+            {
+                // Get the value from the source object
+                object value = property.GetValue(source);
+
+                // Set the value to the target object
+                property.SetValue(target, value);
+            }
+        }
+    }
+
+    /// <summary>
+    ///     Creates a new instance, and copies the properties from the source object into the target object.
+    ///     Only properties that have both a public getter and setter will be copied.
+    /// </summary>
+    /// <param name="source">The source object from which the properties will be copied.</param>
+    public static TDerived CreateFrom<TBase, TDerived>(this TBase source)
+        where TDerived : TBase, new()
+    {
+        if (source == null) throw new ArgumentNullException(nameof(source));
+        var target = new TDerived();
+
+        // Get all public properties from the type T
+        var properties = typeof(TBase).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+        foreach (PropertyInfo property in properties)
+        {
+            // Ensure the property has both a public getter and setter
+            if (property.CanRead && property.CanWrite)
+            {
+                // Get the value from the source object
+                object value = property.GetValue(source);
+
+                // Set the value to the target object
+                property.SetValue(target, value);
+            }
+        }
+        
+        return target;
+    }
+}
