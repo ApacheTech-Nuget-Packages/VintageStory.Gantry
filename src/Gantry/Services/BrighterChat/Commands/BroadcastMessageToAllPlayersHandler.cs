dfe98bebcf563599f605752388c4b9be3d5aa691
ApacheTech Solutions <p_matthews@live.co.uk>
2025-02-22 00:27:40 +0000

    feat: add broadcast to players handler

---
+++ src/Gantry/Services/BrighterChat/Commands/BroadcastMessageToAllPlayersHandler.cs
@@ -0,0 +1,27 @@
+﻿using ApacheTech.Common.BrighterSlim;
+using Gantry.Core.Annotation;
+using Gantry.Services.Brighter.Filters;
+using Vintagestory.API.Server;
+using Vintagestory.Server;
+
+namespace Gantry.Services.BrighterChat.Commands;
+
+/// <summary>
+///     Broadcasts a message to all players on the server.
+/// </summary>
+[ServerSide]
+internal class BroadcastMessageToAllPlayersHandler(ServerMain game) : RequestHandler<BroadcastMessageToAllPlayersCommand>
+{
+    [HandledOnServer]
+    public override BroadcastMessageToAllPlayersCommand Handle(BroadcastMessageToAllPlayersCommand command)
+    {
+        foreach (var player in game.AllOnlinePlayers.Cast<IServerPlayer>())
+        {
+            var message = command.LocaliseForEachPlayer
+                ? Lang.GetL(player.LanguageCode, command.MessageCode, command.Arguments)
+                : Lang.Get(command.MessageCode, command.Arguments);
+            game.SendMessage(player, GlobalConstants.AllChatGroups, message, EnumChatType.Notification);
+        }
+        return base.Handle(command);
+    }
+}
