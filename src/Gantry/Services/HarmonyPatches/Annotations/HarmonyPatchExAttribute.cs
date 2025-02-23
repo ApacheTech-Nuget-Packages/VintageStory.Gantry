577a8b90e5d65f9618fc43e557daf9ae5e7b17ad
ApacheTech Solutions <p_matthews@live.co.uk>
2025-02-23 05:39:04 +0000

    feat: add extended harmony patch attribute

---
+++ src/Gantry/Services/HarmonyPatches/Annotations/HarmonyPatchExAttribute.cs
@@ -0,0 +1,16 @@
+﻿namespace Gantry.Services.HarmonyPatches.Annotations;
+
+/// <summary>
+///     Extends <see cref="HarmonyPatch"/> to provide a shorthand way to define patches with argument types.
+/// </summary>
+public class HarmonyPatchExAttribute : HarmonyPatch
+{
+    /// <summary>
+    ///     Initialises a new instance of the <see cref="HarmonyPatchExAttribute"/> class.
+    /// </summary>
+    /// <param name="declaringType">The full name of the declaring type.</param>
+    /// <param name="methodName">The name of the method to patch.</param>
+    /// <param name="arguments">The argument types of the target method.</param>
+    public HarmonyPatchExAttribute(string declaringType, string methodName, params Type[] arguments)
+        : base(declaringType, methodName) => info.argumentTypes = arguments;
+}
