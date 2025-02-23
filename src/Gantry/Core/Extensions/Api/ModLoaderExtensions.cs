a165a4b06a91b92fb1d2ba8869e148432a340911
ApacheTech Solutions <p_matthews@live.co.uk>
2025-02-23 02:19:57 +0000

    feat: add modloader extensions

---
+++ src/Gantry/Core/Extensions/Api/ModLoaderExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace Gantry.Core.Extensions.Api;
+
+/// <summary>
+///     Provides extension methods for checking the mod loader status.
+/// </summary>
+public static class ModLoaderExtensions
+{
+    /// <summary>
+    ///     Determines whether any of the specified mods are loaded.
+    /// </summary>
+    /// <param name="modLoader">The game's mod loader.</param>
+    /// <param name="modIds">An array of mod identifiers to check.</param>
+    /// <returns>
+    ///     <c>true</c> if any of the specified mods are loaded; otherwise, <c>false</c>.
+    /// </returns>
+    public static bool AreAnyModsLoaded(this IModLoader modLoader, params string[] modIds)
+    {
+        foreach (var modId in modIds)
+        {
+            if (modLoader.IsModEnabled(modId)) return true;
+        }
+        return false;
+    }
+}
