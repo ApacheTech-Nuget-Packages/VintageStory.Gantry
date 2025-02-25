40d96c479fd49af743f2a0c890a25365c5964938
ApacheTech Solutions <p_matthews@live.co.uk>
2025-02-25 21:15:06 +0000

    feat: pull shared packets up to gantry

---
+++ src/Gantry/Services/Network/Packets/WorldMapTeleportPacket.cs
@@ -0,0 +1,16 @@
+﻿using ProtoBuf;
+using Vintagestory.API.MathTools;
+
+namespace Gantry.Services.Network.Packets;
+
+/// <summary>
+///     Represents a packet for teleporting within the world map.
+/// </summary>
+[ProtoContract(ImplicitFields = ImplicitFields.AllPublic)]
+public sealed class WorldMapTeleportPacket
+{
+    /// <summary>
+    ///     Gets or sets the target position for teleportation.
+    /// </summary>
+    public Vec3d Position { get; set; }
+}
---
+++ src/Gantry/Services/Network/Packets/WorldNamePacket.cs
@@ -0,0 +1,15 @@
+﻿using ProtoBuf;
+
+namespace Gantry.Services.Network.Packets;
+
+/// <summary>
+///     Represents a packet containing the world name.
+/// </summary>
+[ProtoContract(ImplicitFields = ImplicitFields.AllPublic)]
+public class WorldNamePacket
+{
+    /// <summary>
+    ///     Gets or sets the name of the world.
+    /// </summary>
+    public string Name { get; set; }
+}
