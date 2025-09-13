1d184986f8ab2700f13c3e009f357376bbeb8ad1
ApacheTech Solutions <p_matthews@live.co.uk>
2025-09-13 16:10:01 +0100

    feat: add command pattern interface

---
+++ src/Gantry/Core/Abstractions/ICommand.cs
@@ -0,0 +1,12 @@
+﻿namespace Gantry.Core.Abstractions;
+
+/// <summary>
+///     Represents a command that can be executed.
+/// </summary>
+public interface ICommand
+{
+    /// <summary>
+    ///     Executes the command.
+    /// </summary>
+    void Execute();
+}
