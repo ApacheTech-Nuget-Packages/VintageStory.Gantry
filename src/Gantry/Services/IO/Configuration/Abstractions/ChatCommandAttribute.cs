7f38a5f1e3669a7704f5fc9eab7d0bf7e76002cf
ApacheTech Solutions <p_matthews@live.co.uk>
2025-10-19 13:42:37 +0100

    feat: add chat command attribute for settings files

---
+++ src/Gantry/Services/IO/Configuration/Abstractions/ChatCommandAttribute.cs
@@ -0,0 +1,38 @@
+﻿namespace Gantry.Services.IO.Configuration.Abstractions;
+
+/// <summary>
+///     Indicates that a property is associated with a chat command.
+/// </summary>
+[AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+public class ChatCommandAttribute(string name) : Attribute
+{
+    /// <summary>
+    ///     The name of the chat command.
+    /// </summary>
+    public string Name { get; } = name;
+
+    /// <summary>
+    ///     The default value for the property.
+    /// </summary>
+    public object? DefaultValue { get; init; }
+
+    /// <summary>
+    ///     The maximum value allowed for the property.
+    /// </summary>
+    public object? MaxValue { get; set; }
+
+    /// <summary>
+    ///     The minimum value allowed for the property.
+    /// </summary>
+    public object? MinValue { get; set; }
+
+    /// <summary>
+    ///     The aliases for the chat command.
+    /// </summary>
+    public string[]? Aliases { get; set; }
+
+    /// <summary>
+    ///     The list of words associated with the chat command.
+    /// </summary>
+    public string[]? WordList { get; set; }
+}
