cbde64d7ecf3f3b7f48bf556a3451181da2f57ac
ApacheTech Solutions <p_matthews@live.co.uk>
2025-10-20 01:00:06 +0100

    feat: add client patch class for easyx mods

---
+++ src/Gantry/Services/EasyX/Abstractions/EaxyXClientPatchClass.cs
@@ -0,0 +1,31 @@
+﻿using Gantry.Core.Abstractions;
+using Gantry.Services.IO.Configuration.Abstractions;
+
+namespace Gantry.Services.EasyX.Abstractions;
+
+/// <summary>
+///     Base class for EasyX client patches.
+/// </summary>
+/// <typeparam name="TClientSystem">The client system type.</typeparam>
+/// <typeparam name="TClientSettings">The client settings type.</typeparam>
+/// <typeparam name="TServerSettings">The server settings type.</typeparam>
+public abstract class EaxyXClientPatchClass<TClientSystem, TClientSettings, TServerSettings>
+    where TClientSystem : EasyXClientSystemBase<TClientSystem, TClientSettings, TServerSettings>
+    where TClientSettings : class, IEasyXClientSettings, new()
+    where TServerSettings : FeatureSettings<TServerSettings>, IEasyXServerSettings, new()
+{
+    /// <summary>
+    ///     The client system instance.
+    /// </summary>
+    protected static TClientSystem ClientSystem => EasyXClientSystemBase<TClientSystem, TClientSettings, TServerSettings>.Instance;
+
+    /// <summary>
+    ///     The client settings instance.
+    /// </summary>
+    protected static TClientSettings Settings => ClientSystem.Settings;
+
+    /// <summary>
+    ///     The core Gantry API.
+    /// </summary>
+    protected static ICoreGantryAPI Core => ClientSystem.Core;
+}
